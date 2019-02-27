1cbf631c1405af4a556741bf02bc9817e3f9c6a4
chenhongguang01 <chenhongguang01@baidu.com>
2019-02-28 02:08:06 +0800

    fix

--- run_eva.sh
+++ run_eva.sh
@@ -2,7 +2,6 @@
 source ~/.bashrc
 
 CUDA_VISIBLE_DEVICES=7 python_chg ner.py --vocab_file="chinese_L-12_H-768_A-12/vocab.txt" \
-                                             --do_train=true \
                                              --output_dir="./output" \
                                              --bert_config_file=chinese_L-12_H-768_A-12/bert_config.json \
                                              --vocab_file=chinese_L-12_H-768_A-12/vocab.txt  \
--- run_train.sh
+++ run_train.sh
@@ -9,5 +9,4 @@ CUDA_VISIBLE_DEVICES=3 python_chg seq2seq.py --vocab_file="chinese_L-12_H-768_A-
                                              --do_train=true \
                                              --output_dir="./output" \
                                              --bert_config_file=chinese_L-12_H-768_A-12/bert_config.json \
-                                             --init_checkpoint=chinese_L-12_H-768_A-12/bert_model.ckpt \
-                                             --do_train=true
+                                             --init_checkpoint=chinese_L-12_H-768_A-12/bert_model.ckpt 
