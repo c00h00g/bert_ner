41f84c0531c8a61854ba4efcf31b19e8ebf8637d
chenhongguang01 <chenhongguang01@baidu.com>
2019-03-01 01:10:02 +0800

    fix

--- run_train.sh
+++ run_train.sh
@@ -5,8 +5,8 @@ if [[ ! -d ./output_t ]]; then
     mkdir output_t
 fi
 
-CUDA_VISIBLE_DEVICES=3 python_chg seq2seq.py --vocab_file="chinese_L-12_H-768_A-12/vocab.txt" \
-                                             --do_train=true \
-                                             --output_dir="./output" \
-                                             --bert_config_file=chinese_L-12_H-768_A-12/bert_config.json \
-                                             --init_checkpoint=chinese_L-12_H-768_A-12/bert_model.ckpt 
+CUDA_VISIBLE_DEVICES=3 python_chg ner.py --vocab_file="chinese_L-12_H-768_A-12/vocab.txt" \
+                                         --do_train=true \
+                                         --output_dir="./output" \
+                                         --bert_config_file=chinese_L-12_H-768_A-12/bert_config.json \
+                                         --init_checkpoint=chinese_L-12_H-768_A-12/bert_model.ckpt 
